7ce61a4005a6084d857132b21e580eb0f17885f1
Erik Fredericks <frederer@arch04.cis.gvsu.edu>
2022-11-17 22:04:54 -0500

    init

---
+++ script_under_test.py
@@ -0,0 +1,18 @@
+# I am the file being developed
+
+import math
+
+def hello_world():
+  return "HELLO THERE"
+
+
+def create_num_list(list_len):
+  return [_ for _ in range(list_len)]
+
+
+def getPi():
+  return math.pi
+
+
+if __name__ == "__main__":
+  print(hello_world())
